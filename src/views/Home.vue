fb9fd32d1e9181415cfb59a4ea4c943f3778c156
Alejandro Vasquez <josejaguer12@gmail.com>
2021-09-07 15:27:41 -0600

    Section 1 realizado

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <anes-section />
+    <anes-history-section />
+  </div>
+</template>
+<script>
+import AnesSection from "@/components/AnesSection.vue";
+import AnesHistorySection from "@/components/AnesHistorySection.vue";
+
+export default {
+  name: "Home",
+  components: { AnesSection, AnesHistorySection },
+};
+</script>
