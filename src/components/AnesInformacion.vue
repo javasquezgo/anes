fbc7c92c9769b411645490dfd69e7557bde8dca0
Alejandro Vasquez <josejaguer12@gmail.com>
2021-09-08 18:36:31 -0600

    Por terminar

---
+++ src/components/AnesInformacion.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    <figure>
+      <img
+        src="@/assets/image/enfermerasX2.jpg"
+        alt="Enfermera
+          "
+      />
+    </figure>
+  </section>
+</template>
+<script>
+export default {};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,7 @@
     <anes-history-section />
     <anes-asociate />
     <anes-comites />
+    <anes-informacion />
   </div>
 </template>
 <script>
@@ -11,9 +12,16 @@ import AnesSection from "@/components/AnesSection.vue";
 import AnesHistorySection from "@/components/AnesHistorySection.vue";
 import AnesAsociate from "@/components/AnesAsociate.vue";
 import AnesComites from "@/components/AnesComites.vue";
+import AnesInformacion from "@/components/AnesInformacion.vue";
 
 export default {
   name: "Home",
-  components: { AnesSection, AnesHistorySection, AnesAsociate, AnesComites },
+  components: {
+    AnesSection,
+    AnesHistorySection,
+    AnesAsociate,
+    AnesComites,
+    AnesInformacion,
+  },
 };
 </script>
