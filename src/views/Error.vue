d41f8a41f6934f2712c4b265c23bc9555f9aed85
Alejandro Vasquez <josejaguer12@gmail.com>
2021-09-09 14:29:12 -0600

    Se empezo a modificar el router

---
+++ src/views/Error.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="flex flex-col items-center justify-center h-96">
+    <h2 class="text-3xl text-gray-900">Esta pagina no existe :(</h2>
+    <router-link to="/">Volver a inicio</router-link>
+  </div>
+</template>
