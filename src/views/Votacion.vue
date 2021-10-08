9e828a0db4c6e0040aeefa3c8f2e5cdad42d5d20
Alejandro Vasquez <josejaguer12@gmail.com>
2021-10-07 20:17:14 -0600

    fixing typos

--- src/views/Votacion.vue
+++ src/views/Votacion.vue
@@ -3,7 +3,7 @@
     class="flex flex-col items-center justify-center pb-20 text-center"
     id="votar"
   >
-    <h2 class="mt-16 mb-10 text-5xl text-gray-title">Planillas</h2>
+    <h2 class="mt-16 mb-10 text-5xl text-gray-title">Comité Electoral</h2>
 
     <img
       class="w-11/12 mb-10 md:w-7/12 lg:w-4/12 md:mb-20"
@@ -15,14 +15,7 @@
 
     <a href="https://dev1.heavywebdesign.com/2pixanes/elecciones"
       ><span
-        class="
-          px-10
-          py-3
-          rounded-lg
-          hover:shadow-xl
-          bg-blue-soft
-          text-white-bone
-        "
+        class="px-10 py-3 rounded-lg  hover:shadow-xl bg-blue-soft text-white-bone"
         >Enlace de Votación</span
       ></a
     >
@@ -31,14 +24,7 @@
 
     <router-link to="/planilla"
       ><span
-        class="
-          px-20
-          py-3
-          rounded-lg
-          hover:shadow-xl
-          bg-blue-soft
-          text-white-bone
-        "
+        class="px-20 py-3 rounded-lg  hover:shadow-xl bg-blue-soft text-white-bone"
       >
         Planillas</span
       ></router-link
